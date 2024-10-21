f0188917fa258ac0d08e61ab0d7985eed711147f
Vanraj Pardeshi <truecapturers@gmail.com>
2024-10-21 11:02:08 +0530

    Fixed the model training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,41 +42,44 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbors Classifier": KNeighborsRegressor(),
-                "XGBClassifier": XGBRegressor(),
-                "CatBoosting Classifier": CatBoostRegressor(verbose=False),
-                "AdaBoost Classifier": AdaBoostRegressor(),
+                "K-Neighbors Regressor": KNeighborsRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
             params = {
+                "Random Forest": {"n_estimators": [8, 16, 32, 64, 128, 256]},
                 "Decision Tree": {
-                    'criterion': ['squared_error','friedman_mse','absolute_error','poisson'],
-                },
-                "Random Forest": {
-                    'n_estimators': [8,16,32,64,128,256]
+                    "criterion": [
+                        "squared_error",
+                        "friedman_mse",
+                        "absolute_error",
+                        "poisson",
+                    ],
                 },
                 "Gradient Boosting": {
-                    'learning_rate': [.1,.01,.05,.001],
-                    'subsample': [0.6,0.7,0.75,0.8,0.85,0.9],
-                    'n_estimators': [8,16,32,64,128,256]
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
                 },
                 "Linear Regression": {},
-                "K-Neighbour Regression": {
-                    'n_neighbors': [5,7,9,11],
+                "K-Neighbors Regressor": {
+                    "n_neighbors": [5, 7, 9, 11],
                 },
                 "XGBRegressor": {
-                    'learning_rate': [.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
                 },
                 "CatBoosting Regressor": {
-                    'depth': [6,8,10],
-                    'learning_rate': [0.01,0.05,0.1],
-                    'iterations': [30,50,100],
+                    "depth": [6, 8, 10],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "iterations": [30, 50, 100],
                 },
                 "AdaBoost Regressor": {
-                    'learning_rate': [.1,.01,0.5,0.001],
-                    'n_estimators': [8,16,32,64,128,256]
-                }
+                    "learning_rate": [0.1, 0.01, 0.5, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
             }
             model_report: dict = evaluate_model(
                 X_train=X_train,
@@ -84,7 +87,7 @@ class ModelTrainer:
                 X_test=X_test,
                 y_test=y_test,
                 models=models,
-                param=params
+                param=params,
             )
 
             ## To get best model score from dict
--- src/utils.py
+++ src/utils.py
@@ -32,16 +32,20 @@ def evaluate_model(X_train,y_train,X_test,y_test,models,param):
 
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
-            # model.fit(X_train,y_train) # Train the model
+            
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             
             train_model_score = r2_score(y_train,y_train_pred)
-
             test_model_score = r2_score(y_test,y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
+        
+        if not report:  # Check if the report is empty
+            raise ValueError("No models were successfully evaluated")
+        
         return report
-    except:
-        pass
+    except Exception as e:
+        logging.error(f"Error in evaluate_model: {str(e)}")
+        raise CustomException(e, sys)
 
